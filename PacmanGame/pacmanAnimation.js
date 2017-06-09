f05c6d6fa19ea7686b4b75ca5b2d58ab42ba9eb9
trapeznikova-evgenija <trapeznikova.evg@yandex.ru>
2017-06-09 14:01:14 +0300

    +labirint and move packman

--- PacmanGame/pacmanAnimation.js
+++ PacmanGame/pacmanAnimation.js
@@ -1,128 +1,195 @@
-//var canvas = document.getElementById("canvas");
-//var context = canvas.getContext("2d");
+var canvas;
+var context;
 window.onload = function () {
 
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
+  canvas = document.getElementById("canvas");
+  context = canvas.getContext("2d");
 
   var CANVAS_WIDTH = 1000;
-  var CANVAS_HEIGHT = 800;
+  var CANVAS_HEIGHT = 500;
 
   canvas.width = CANVAS_WIDTH;
   canvas.height = CANVAS_HEIGHT;
 
-  drawMap();
-  function drawMap() {
-    context.beginPath();
-    context.strokeStyle = "blue";
-    context.lineWidth = 3;
-    context.strokeRect(0, 0, 200, 400);
-    context.stroke();
+
+   initTick(context);
+
+
+   function initTick(context) {
+     var packman = new Packman(267, 4, 8, "green", 0, 0);
+     animationTick();
+     function animationTick() {
+       drawMaze("maze.png", 0, 0);
+       packman.calcSmileAngle();
+       drawPackman(context, packman);
+       //packman._x += 2;
+       function handler(event) {
+         packman.dx = 0;
+         packman.dy = 0;
+
+         switch (event.keyCode) {
+           case keys['RIGHT']:
+             packman.dx = 2;
+             break;
+           case keys['DOWN']:
+             packman.dy = 2;
+             break;
+           case keys['UP']:
+             packman.dy = -2;
+             break;
+           case keys['LEFT']:
+             packman.dx = -2;
+             break;
+         }
+       }
+       
+       function redraw(packman) {
+         if (packman.dx != 0) {
+           packman._x += packman.dx;
+         }
+
+         if (packman.dy != 0 ) {
+            packman._y += packman.dy
+         }
+
+         if (packman._y > 593) {
+            alert('Ты победил!');
+         }
+       }
+
+       redraw(packman);
+
+
+       if (checkCollisions(packman)) {
+         packman._x -= packman.dx;
+         packman._y -= packman.dy;
+         packman.dx = 0;
+         packman.dy = 0;
+
+       }
+
+       addEventListener("keydown", handler);
+       window.requestAnimationFrame(animationTick);
+     }
+
+   }
+
+  function drawMaze(mazeFile) {
+
+    var imgMaze = new Image();
+    imgMaze.onload = function () {
+      canvas.width = imgMaze.width;
+      canvas.height = imgMaze.height;
+
+      context.drawImage(imgMaze, 0, 0);
+    };
+    imgMaze.src = mazeFile;
   }
 
-  initTick(context);
-
-
-  function initTick(context) {
-    var packman = new Packman(100, 150, 100, "#efef11");
-    animationTick();
-
-    function animationTick() {
-      packman.calcSmileAngle();
-      context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-      drawPackman(context, packman);
-      window.requestAnimationFrame(animationTick);
-    }
-
+  function drawFood(foodFile) {
+    var imgFood = new Image();
+    imgFood.onload = function () {
+      context.drawImage(imgFood, 30, 40);
+    };
+    imgFood.src = foodFile;
   }
-  function drawPackmanFigure(context, x, y, radius, color, endAngle) {
+  
+  function checkCollisions(packman) {
+    var imageData = context.getImageData(packman._x - 1, packman._y - 1, 15 + 2, 15 + 2);
+    var pixels = imageData.data;
 
-    var startPoint = {
-      x: x + Math.cos(0) * radius,
-      y: y + Math.sin(0) * radius
-    };
+    for (var i = 0; n = pixels.length, i < n; i += 4) {
+      var red = pixels[i];
+      var green = pixels[i + 1];
+      var blue = pixels[i + 2];
 
-    var endPoint = {
-      x: x + Math.cos(endAngle) * radius,
-      y: y + Math.sin(endAngle) * radius
-    };
-
-    context.fillStyle = color;
-    context.beginPath();
-
-    //вырез рта
-    context.moveTo(x, y);
-    context.lineTo(startPoint.x, startPoint.y);
-    context.moveTo(x, y);
-    context.lineTo(endPoint.x, endPoint.y);
-
-    //теперь тело
-    context.arc(x, y, radius, endAngle, 0, false);
-    context.fillStyle = color;
-    context.strokeStyle = color;
-    context.fill();
-    context.stroke();
-    context.closePath();
-  }
-
-
-  function Packman(x, y, radius, color)
-  {
-    this.COLOR = color;
-    this.RADIUS = radius;
-
-    this._x = x;
-    this._y = y;
-    this._smileAngle = 1;
-    this._smileAngleStep = 5;
-
-    this.getX = function ()
-    {
-      return this._x;
-    };
-    this.getY = function ()
-    {
-      return this._y;
-    };
-    this.getSmileAngle = function()
-    {
-      return this._smileAngle;
-    };
-    this.calcSmileAngle = function ()
-    {
-      this._smileAngle += this._smileAngleStep;
-      if ((this._smileAngle >= 90) || (this._smileAngle <= 1))
-      {
-        this._smileAngleStep = - this._smileAngleStep;
+      if (red == 0 && green == 0 && blue == 0) {
+        return true;
       }
-    };
+      if (red == 169 && green == 169 && blue == 169) {
+        return true;
+      }
+    }
+    return false;
   }
 
+   function drawPackmanFigure(context, x, y, radius, color, endAngle) {
 
-  function degToRad(deg) {
-    inDeg = (Math.PI / 180) * deg;
-    return inDeg;
-  }
+     var startPoint = {
+       x: x + Math.cos(0) * radius,
+       y: y + Math.sin(0) * radius
+     };
 
-  function drawPackman(context, Packman) {
-    var cordX = Packman.getX() + Packman.RADIUS;
-    var cordY = Packman.getY() + Packman.RADIUS;
-    var smileAngle = degToRad(Packman.getSmileAngle());
+     var endPoint = {
+       x: x + Math.cos(endAngle) * radius,
+       y: y + Math.sin(endAngle) * radius
+     };
 
-    context.translate(cordX, cordY);
-    context.rotate(-smileAngle / 2);
-    drawPackmanFigure(context, 0, 0, Packman.RADIUS, Packman.COLOR, smileAngle);
-    context.rotate(smileAngle / 2);
-    context.translate(-cordX, -cordY);
-    Packman.isOpened = !Packman.isOpened;
-  }
+     context.fillStyle = color;
+     context.beginPath();
+
+     //вырез рта
+     context.moveTo(x, y);
+     context.lineTo(startPoint.x, startPoint.y);
+     context.moveTo(x, y);
+     context.lineTo(endPoint.x, endPoint.y);
+
+     //теперь тело
+     context.arc(x, y, radius, endAngle, 0, false);
+     context.fillStyle = color;
+     context.strokeStyle = color;
+     context.fill();
+     context.stroke();
+     context.closePath();
+   }
 
 
+   function Packman(x, y, radius, color, dx, dy) {
+     this.COLOR = color;
+     this.RADIUS = radius;
+
+     this.dx = dx;
+     this.dy = dy;
+
+     this._x = x;
+     this._y = y;
+     this._smileAngle = 1;
+     this._smileAngleStep = 5;
+
+     this.getX = function () {
+       return this._x;
+     };
+     this.getY = function () {
+       return this._y;
+     };
+     this.getSmileAngle = function () {
+       return this._smileAngle;
+     };
+     this.calcSmileAngle = function () {
+       this._smileAngle += this._smileAngleStep;
+       if ((this._smileAngle >= 90) || (this._smileAngle <= 1)) {
+         this._smileAngleStep = -this._smileAngleStep;
+       }
+     };
+   }
+
+
+   function degToRad(deg) {
+     inDeg = (Math.PI / 180) * deg;
+     return inDeg;
+   }
+
+   function drawPackman(context, Packman) {
+     var cordX = Packman.getX() + Packman.RADIUS;
+     var cordY = Packman.getY() + Packman.RADIUS;
+     var smileAngle = degToRad(Packman.getSmileAngle());
+
+     context.translate(cordX, cordY);
+     context.rotate(-smileAngle / 2);
+     drawPackmanFigure(context, 0, 0, Packman.RADIUS, Packman.COLOR, smileAngle);
+     context.rotate(smileAngle / 2); 
+     context.translate(-cordX, -cordY);
+     Packman.isOpened = !Packman.isOpened;
+     
+   }
 };
-
-addEventListener("keydown", function (event) {
-  if (event.keyCode == 39) {
-    alert(event.type);
-  }
-
-});
